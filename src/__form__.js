6cd97ad361413a510cee5a8d74b79c7b48fd85f1
shoyonp <shoyonkumar143@gmail.com>
2024-10-31 00:23:33 +0600

    on change

--- src/__form__.js
+++ src/__form__.js
@@ -1,3 +1,5 @@
 /**
- * 1.  use form tag anad name to get values
+ * 1.  use form tag and onSubmit handler with event (e) From e access e.traget.[name of the input field].value
+ * 
+ * 2. use individual field state
  */
--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,9 @@
 const SimpleForm = () => {
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(e.target.name.value);
-    console.log(e.target.email.value);
-    console.log(e.target.phone.value);
+    // console.log(e.target.name.value);
+    // console.log(e.target.email.value);
+    // console.log(e.target.phone.value);
     console.log("form submitted");
   };
   return (
