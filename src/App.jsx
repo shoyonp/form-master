f5eb9a4e945f016736598d30ed08671331ae4f2d
shoyonp <shoyonkumar143@gmail.com>
2024-10-31 01:03:37 +0600

    refForm done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
-import StatefulForm from "./components/StatefulForm/StatefulForm";
+import RefForm from "./components/RefForm/RefForm";
+// import SimpleForm from "./components/SimpleForm/SimpleForm";
+// import StatefulForm from "./components/StatefulForm/StatefulForm";
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
-      <StatefulForm></StatefulForm>
+      {/* <StatefulForm></StatefulForm> */}
+      <RefForm></RefForm>
     </>
   );
 }
--- src/__form__.js
+++ src/__form__.js
@@ -1,5 +1,10 @@
 /**
  * 1.  use form tag and onSubmit handler with event (e) From e access e.traget.[name of the input field].value
- * 
- * 2. use individual field state
+ *
+ * 2.controlled element : use individual field state for each input field
+ *
+ *
+ * 3. controlled element: one object i a state holding all the input fields value
+ *
+ * 4. uncontrolled element: useRef to creat a reference to the element adn access value by using like: nameRef.current.value
  */
